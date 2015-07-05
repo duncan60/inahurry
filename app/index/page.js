35e69a8031b777856bfed592cae65fbc061b1dcf
duncan60 <tmduncan60@gmail.com>
2015-07-05 19:28:40 +0800

    add get geolocation

--- app/index/page.js
+++ app/index/page.js
@@ -4,12 +4,34 @@ import CommonChild from '../common/common-child';
 class Page extends BaseComponent {
     constructor(props) {
         super(props);
-        this._bind();
+        this._bind(
+            '_getGeolocation'
+        );
+        this.state = {
+            latitude : 0,
+            longitude: 0
+        }
+    }
+    componentWillMount() {
+        this._getGeolocation();
+    }
+    _getGeolocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.setState({
+                    latitude : position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            });
+        } else {
+
+        }
     }
     render() {
         return (
             <div>
-                Hello INDEX!!!
+                <p>latitude : {this.state.latitude}</p>
+                <p>longitude: {this.state.longitude}</p>
                 <CommonChild />
             </div>
         );
