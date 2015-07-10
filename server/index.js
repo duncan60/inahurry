3c708678c38acfa804ba8a631f9cfce32e9cc81e
duncan60 <tmduncan60@gmail.com>
2015-07-10 14:06:38 +0800

    add trams date file

--- server/index.js
+++ server/index.js
@@ -8,29 +8,29 @@ import bundle from'./webpack-server';
 
 let app = express();
 
-bundle();
+// bundle();
 
-let renderPage = (entryPath) => {
-    return React.renderToString(
-                React.createElement(
-                    DefaultPage,
-                    {jsPath: entryPath}
-                )
-            )
-}
+// let renderPage = (entryPath) => {
+//     return React.renderToString(
+//                 React.createElement(
+//                     DefaultPage,
+//                     {jsPath: entryPath}
+//                 )
+//             )
+// }
 
 app.get('/', (req, res) => {
    res.end(renderPage('//localhost:8080/build/index.js'));
 });
 
 app.route('/api/trains')
-	.get((req, res) => {
-		res.json({
-			latitude : req.query.latitude,
-			longitude: req.query.longitude,
-			message  : "第一個API!"
-		});
-	});
+    .get((req, res) => {
+        res.json({
+            latitude : req.query.latitude,
+            longitude: req.query.longitude,
+            message  : "第一個API!"
+        });
+    });
 
 app.listen(3000, () => {
    opn('http://localhost:3000');
