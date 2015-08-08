8e8dbe30f340ae47c4655f3e65d733a12646d89d
duncan60 <tmduncan60@gmail.com>
2015-08-08 23:08:14 +0800

    refine crawl api, use es6 promise

--- server/crawl-trains.js
+++ server/crawl-trains.js
@@ -3,47 +3,69 @@ import cheerio from 'cheerio';
 import moment from 'moment';
 
 let crawlTrains =  {
-	getTrainsData(stations, callback) {
-		this.stations = stations;
-		this.postRequest('north', callback);
-		this.postRequest('south', callback);
-	},
-
-	templateURL(type) {
-		const {targetStation, northStation, southStation} = this.stations;
-		let momentDate    = moment(),
-			searchDate    =  momentDate.format('YYYY/MM/DD'),
-			fromCityId    =  targetStation.city,
-			fromStationId =  targetStation.id,
-			toCityId      =  type === 'north' ? northStation.city : southStation.city,
-			toStationId   =  type === 'north' ? northStation.id : southStation.id,
-			nowTime       = momentDate.format('HHmm'),
-			totime        = momentDate.add(1, 'hours').format('HH') === '00' ? '24' + momentDate.format('mm') : momentDate.format('HHmm');
-		return `http://twtraffic.tra.gov.tw/twrail/SearchResult.aspx?searchtype=0&searchdate=${searchDate}&fromcity=${fromCityId}&tocity=${toCityId}&fromstation=${fromStationId}&tostation=${toStationId}&trainclass=2&fromtime=${nowTime}&totime=${totime}`;
-	},
-	postRequest(type, callback) {
-		request({
-	        url   : this.templateURL(type),
-	        method: 'GET'
-	    }, (e, r, b) => {
-	    	//console.log('url', this.templateURL(type));
-	    	if (e || !b) { return; }
-	    	let $        = cheerio.load(b),
-	        	result   = [],
-	        	train    = {},
-	        	trainRow = $('.Grid_Row'),
-	        	$trainTarget;
-	        for (var i = 1; i < trainRow.length; i++) {
-	            train = {};
-	            $trainTarget = $(trainRow[i]);
-	            train.type = $trainTarget.find('.SearchResult_TrainType span').text();
-	            train.startTime = $trainTarget.find('.SearchResult_Time').eq(0).text();
-	            train.router = $trainTarget.find('td').eq(3).text();
-	            result.push(train);
-	        }
-	        callback(type, result);
-	    });
-	}
+    getTrainsData(stations, callback) {
+        this.model = {
+            code : -1,
+            north: [],
+            south: [],
+            msg  : ''
+        };
+        let north = this.promise(this.templateURL(stations, 'north'), 'GET'),
+            south = this.promise(this.templateURL(stations, 'south'), 'GET');
+        Promise.all([north, south]).then((res) => {
+            this.model.north = this.parseData(res[0]);
+            this.model.south = this.parseData(res[1]);
+            this.model.code = 0;
+            this.model.msg = 'success';
+            callback(this.model);
+        }, (error) => {
+            this.model.code = 1;
+            this.model.msg = error;
+            callback(this.model);
+        });
+    },
+    promise(url, type) {
+        return new Promise((resolve, reject) => {
+            request({
+                url   : url,
+                method: type
+            }, (e, r, b) => {
+                if (e || !b) {
+                    reject();
+                } else {
+                    resolve(b);
+                }
+            });
+        });
+    },
+    templateURL(stations, type) {
+        const {targetStation, northStation, southStation} = stations;
+        let momentDate    = moment(),
+            searchDate    = momentDate.format('YYYY/MM/DD'),
+            fromCityId    = targetStation.city,
+            fromStationId = targetStation.id,
+            toCityId      = type === 'north' ? northStation.city : southStation.city,
+            toStationId   = type === 'north' ? northStation.id : southStation.id,
+            nowTime       = momentDate.format('HHmm'),
+            totime        = momentDate.add(1, 'hours').format('HH') === '00' ? '24' + momentDate.format('mm') : momentDate.format('HHmm');
+        return `http://twtraffic.tra.gov.tw/twrail/SearchResult.aspx?searchtype=0&searchdate=${searchDate}&fromcity=${fromCityId}&tocity=${toCityId}&fromstation=${fromStationId}&tostation=${toStationId}&trainclass=2&fromtime=${nowTime}&totime=${totime}`;
+    },
+    parseData(data) {
+        let $        = cheerio.load(data),
+            result   = [],
+            train    = {},
+            trainRow = $('.Grid_Row'),
+            $trainTarget;
+        for (var i = 1; i < trainRow.length; i++) {
+            train = {};
+            $trainTarget = $(trainRow[i]);
+            train.type = $trainTarget.find('.SearchResult_TrainType span').text();
+            train.startTime = $trainTarget.find('.SearchResult_Time').eq(0).text();
+            train.router = $trainTarget.find('td').eq(3).text();
+            result.push(train);
+        }
+        return result;
+    }
 };
 
 /* parameters rule
--- server/index.js
+++ server/index.js
@@ -22,27 +22,35 @@ let renderPage = (entryPath) => {
 };
 
 app.get('/', (req, res) => {
-	res.end(renderPage('//localhost:8080/build/index.js'));
+    res.end(renderPage('//localhost:8080/build/index.js'));
 });
 
 app.route('/api/trains')
     .get((req, res) => {
-	    let trainsTimetableData = {};
-		let closestTrains = closestTrainStations.search(req.query.latitude, req.query.longitude);
-		crawlTrains.getTrainsData(closestTrains, (type, trainsData) => {
-			trainsTimetableData[type] = trainsData;
-			if (trainsTimetableData.north !== undefined && trainsTimetableData.south !== undefined) {
-				res.json({
-	            	data : {
-	            		trainsTimetableData: trainsTimetableData,
-	            		closestTrains      : closestTrains
-	            	},
-	            	message: 'search trains ok!'
-	        	});
-			}
-		});
+        let closestTrains = closestTrainStations.search(req.query.latitude, req.query.longitude);
+        crawlTrains.getTrainsData(closestTrains, (trainsData) => {
+            if (trainsData.code === 0) {
+                res.json({
+                    data   : {
+                        trainsTimetableData: trainsData,
+                        closestTrains      : closestTrains
+                    },
+                    code   : 0,
+                    message: 'search trains ok!'
+                });
+            } else {
+                res.json({
+                    data   : {
+                        trainsTimetableData: trainsData,
+                        closestTrains      : closestTrains
+                    },
+                    code   : 1,
+                    message: 'search trains error!'
+                });
+            }
+        });
     });
 
 app.listen(3000, () => {
-  	opn('http://localhost:3000');
+    //opn('http://localhost:3000');
 } );
