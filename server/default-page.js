88c3c45e4168030b554f41ec3e6a992b8b187e2d
DuDuncan <tmduncan60@gmail.com>
2015-08-31 17:23:52 +0800

    update meta description

--- server/default-page.js
+++ server/default-page.js
@@ -15,7 +15,7 @@ class DefaultPage extends React.Component {
                     <meta property="og:locale" content="zh_TW" />
                     <meta property="og:url" content="http://inahurry.com.tw/" />
                     <meta property="og:title" content="特搜時刻表" />
-                    <meta property="og:description" content="你你是急急忙忙的通勤族嗎？利用定位分析出距離最近的車站，快速幫你搜尋鐵路時刻表資訊，讓你當個不疾不徐的通勤族" />
+                    <meta property="og:description" content="你是急急忙忙的通勤族嗎？利用定位分析出距離最近的車站，快速幫你搜尋鐵路時刻表資訊，讓你當個不疾不徐的通勤族" />
                     <meta property="article:author" content="https://github.com/duncan60" />
                     <meta property="article:section" content="javascript" />
                     <link href='http://fonts.googleapis.com/css?family=Roboto+Mono:400,300italic' rel='stylesheet' type='text/css' />
