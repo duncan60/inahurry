a0c8e05d5431d26a4d72ec1b7cc1a33ca2ce998d
DuDuncan <tmduncan60@gmail.com>
2015-09-07 16:24:35 +0800

    used to Async Functions

--- server/thsrc/crawl-trains.js
+++ server/thsrc/crawl-trains.js
@@ -1,41 +1,45 @@
 import request from 'request';
+import cheerio from 'cheerio';
+import moment from 'moment';
 
 let crawlTrains =  {
-    getTrainsData(stations, callback) {
+    async getTrainsData(stations, callback) {
         this.model = {
             code : -1,
             north: [],
             south: [],
             msg  : ''
         };
-        let getStationInfo = this.promise(stations.targetStation, 'GET');
+
+       // let getStationInfo = this.promise(stations.targetStation, 'GET');
         //console.log('stations', stations);
-        Promise.all([getStationInfo]).then(
-            (res) => {
-                let resJson = {};
-                resJson = JSON.parse(JSON.parse(res[0]));
-                this.model.north = resJson['northbound'].map((item) => {
-                    return {
-                        trainNumber  : item.TrainNumber,
-                        departureTime: item.DepartureTime
-                    };
-                });
-                this.model.south = resJson.southbound.map((item) => {
-                    return {
-                        trainNumber  : item.TrainNumber,
-                        departureTime: item.DepartureTime
-                    };
-                });
-                this.model.code = 0;
-                this.model.msg = 'success';
-                callback(this.model);
-            }, (error) => {
-                this.model.code = 1;
-                this.model.msg = error;
-                callback(this.model);
+        //let result = await Promise.all([getStationInfo]);
+        let result = await this.promise(stations.targetStation, 'GET');
+        let resJson = {};
+        try {
+            resJson = JSON.parse(JSON.parse(result));
+            this.model.north = resJson['northbound'].map((item) => {
+                return {
+                    trainNumber  : item.TrainNumber,
+                    departureTime: item.DepartureTime
+                };
             });
+            this.model.south = resJson.southbound.map((item) => {
+                return {
+                    trainNumber  : item.TrainNumber,
+                    departureTime: item.DepartureTime
+                };
+            });
+            this.model.code = 0;
+            this.model.msg = 'success';
+            callback(this.model);
+        } catch(error) {
+            this.model.code = -1;
+            this.model.msg = error;
+            callback(this.model);
+        }
     },
-    promise(targetStaton, type) {
+    async promise(targetStaton, type) {
         return new Promise((resolve, reject) => {
             request({
                 url   : `http://www.thsrc.com.tw/tw/StationInfo/LatestTrainTime?id=${targetStaton.id}`,
