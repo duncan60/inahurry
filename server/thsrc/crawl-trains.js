6c6ef42844b703d7005bfdeadde643a720430ac9
DuDuncan <tmduncan60@gmail.com>
2015-09-07 11:16:19 +0800

    refine code convention

--- server/thsrc/crawl-trains.js
+++ server/thsrc/crawl-trains.js
@@ -1,6 +1,4 @@
 import request from 'request';
-import cheerio from 'cheerio';
-import moment from 'moment';
 
 let crawlTrains =  {
     getTrainsData(stations, callback) {
@@ -12,29 +10,30 @@ let crawlTrains =  {
         };
         let getStationInfo = this.promise(stations.targetStation, 'GET');
         //console.log('stations', stations);
-        Promise.all([getStationInfo]).then((res) => {
-            let resJson = {};
-            resJson = JSON.parse(JSON.parse(res[0]));
-            this.model.north = resJson['northbound'].map((item) => {
-                return {
-                    trainNumber  : item.TrainNumber,
-                    departureTime: item.DepartureTime
-                };
+        Promise.all([getStationInfo]).then(
+            (res) => {
+                let resJson = {};
+                resJson = JSON.parse(JSON.parse(res[0]));
+                this.model.north = resJson['northbound'].map((item) => {
+                    return {
+                        trainNumber  : item.TrainNumber,
+                        departureTime: item.DepartureTime
+                    };
+                });
+                this.model.south = resJson.southbound.map((item) => {
+                    return {
+                        trainNumber  : item.TrainNumber,
+                        departureTime: item.DepartureTime
+                    };
+                });
+                this.model.code = 0;
+                this.model.msg = 'success';
+                callback(this.model);
+            }, (error) => {
+                this.model.code = 1;
+                this.model.msg = error;
+                callback(this.model);
             });
-            this.model.south = resJson.southbound.map((item) => {
-                return {
-                    trainNumber  : item.TrainNumber,
-                    departureTime: item.DepartureTime
-                };
-            });
-            this.model.code = 0;
-            this.model.msg = 'success';
-            callback(this.model);
-        }, (error) => {
-            this.model.code = 1;
-            this.model.msg = error;
-            callback(this.model);
-        });
     },
     promise(targetStaton, type) {
         return new Promise((resolve, reject) => {
