978dbeffa46364b5c7f138f506b350441e138711
DuDuncan <tmduncan60@gmail.com>
2015-09-08 10:35:52 +0800

    refine code convention

--- server/thsrc/crawl-trains.js
+++ server/thsrc/crawl-trains.js
@@ -1,6 +1,4 @@
 import request from 'request';
-import cheerio from 'cheerio';
-import moment from 'moment';
 
 let crawlTrains =  {
     async getTrainsData(stations, callback) {
@@ -11,14 +9,10 @@ let crawlTrains =  {
             msg  : ''
         };
 
-       // let getStationInfo = this.promise(stations.targetStation, 'GET');
-        //console.log('stations', stations);
-        //let result = await Promise.all([getStationInfo]);
         let result = await this.promise(stations.targetStation, 'GET');
-        let resJson = {};
         try {
-            resJson = JSON.parse(JSON.parse(result));
-            this.model.north = resJson['northbound'].map((item) => {
+            let resJson = JSON.parse(JSON.parse(result));
+            this.model.north = resJson.northbound.map((item) => {
                 return {
                     trainNumber  : item.TrainNumber,
                     departureTime: item.DepartureTime
@@ -39,7 +33,7 @@ let crawlTrains =  {
             callback(this.model);
         }
     },
-    async promise(targetStaton, type) {
+    promise(targetStaton, type) {
         return new Promise((resolve, reject) => {
             request({
                 url   : `http://www.thsrc.com.tw/tw/StationInfo/LatestTrainTime?id=${targetStaton.id}`,
