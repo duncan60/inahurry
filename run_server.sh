dd2f6c250837b8699aed86de6174c70b64c5c60d
duncan_du <tmduncan60@gmail.com>
2017-01-17 15:41:13 +0800

    remove .sh

--- run_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run -t -i -d \
-       -p 0.0.0.0:8080:8080 \
-       -v $(pwd):/var/www/inahurry \
-       --name inahurry node:6.9.2 \
-       # /bin/bash -c 'cd /var/www/inahurry && sh build.sh'
